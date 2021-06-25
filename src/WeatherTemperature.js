2440fe6e17bc595bc6ec66794bec2503c62c05e6
huldapals <huldapals94@gmail.com>
2021-06-25 15:39:31 +0200

    Added conversion to Fahrenheit

--- src/WeatherTemperature.js
+++ src/WeatherTemperature.js
@@ -1,10 +1,30 @@
-import React from "react";
+import React, {useState} from "react";
 
 export default function WeatherTemperature(props) {
+const [unit, setUnit] = useState("celsius")
+function convertToF(event){
+  event.preventDefault();
+  setUnit("fahrenheit");
+}
+
+function convertToC(event){
+  event.preventDefault();
+  setUnit("celsius");
+}
+if( unit === `celsius`) {
   return (
     <span>
       <span className="current-temperature">{props.celsius}</span>
-      <span className="units">°C </span>
+      <span className="units">°C|<a href="/" onClick={convertToF}>°F</a></span> 
     </span>
   );
+  } else {
+    let F = (props.celsius * 9/5) + 32 
+    return (
+    <span>
+      <span className="current-temperature">{F}</span>
+      <span className="units"><a href="/" onClick={convertToC}>°C</a>|°F</span> 
+    </span>
+  );
+  }
 }
